56785a48050c23d01a71d767a6954e07f7424692
Naushad Ansari <naushadansari@Naushads-MacBook-Air.local>
2025-11-16 20:28:47 +0530

    Product Added

--- collections/js/products.js
+++ collections/js/products.js
@@ -1,1 +1,27 @@
-const products = [];
+const products = [
+    {
+        "id": 1,
+        "name": "Gshock GM-2110D-2A Generic - Silver black",
+        "slug": "gshock-gm-2110d-2a-generic-silver-black",
+        "sku": "g-shock-metal-1",
+        "brand": "G-Shock",
+        "badge": "HOT",
+        "availability": "In Stock",
+        "description": "7 months machine warranty\r\n6 Days Return Policy\r\nCash On Delivery Available",
+        "details": "7 months machine warranty\r\n6 Days Return Policy\r\nCash On Delivery Available",
+        "price": {
+            "old": "₹2999",
+            "new": "₹1699"
+        },
+        "images": {
+            "main": "images/products/watch/1/main.webp",
+            "hover": "images/products/watch/1/hover.webp"
+        },
+        "otherImages": [
+            "images/products/watch/1/other1.webp",
+            "images/products/watch/1/other2.webp"
+        ],
+        "whatsappUrl": "https://wa.me/918097949357?text=Hi!%20I%20want%20to%20order:%20Gshock%20GM-2110D-2A%20Generic%20-%20Silver%20black%20(SKU%20g-shock-metal-1)",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Gshock%20GM-2110D-2A%20Generic%20-%20Silver%20black%0ASKU%3A%20g-shock-metal-1%0APrice%3A%20%E2%82%B91699%0AAvailability%3A%20In%20Stock%0A%0APlease%20confirm%20my%20order."
+    }
+];
