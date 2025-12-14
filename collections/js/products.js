c312d93c06d0faea47f88ed6439fd9504039295c
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-14 14:38:15 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -1832,5 +1832,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Bvlgari%20%20Serpenti%20Women%27s%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Bvlgari%20%20Serpenti%20Women%27s%20Generic%20%0ASKU%3A%20Snak%0APrice%3A%20%E2%82%B92299%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Calvin Klein Burst Analog Women's Generic ",
+        "slug": "calvin-klein-burst-analog-women-s-generic-6b9853666ec9",
+        "sku": "Ck",
+        "brand": "Calvin Klein",
+        "badge": "Hot",
+        "availability": "In stock ",
+        "description": "Brand – Calvin Klein\r\n# For – Her\r\n# Quality – 7AA Premium Collection\r\n# Model – Burst Analog\r\n# Case Size – 32mm\r\n# Warranty – 1 Year Machine Warranty",
+        "details": "",
+        "tags": "women-chain, women-formal, ",
+        "price": {
+            "old": "₹2300",
+            "new": "₹1499"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-burst-analog-women-s-generic-6b9853666ec9/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-burst-analog-women-s-generic-6b9853666ec9/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-burst-analog-women-s-generic-6b9853666ec9/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Calvin%20Klein%20Burst%20Analog%20Women%27s%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Calvin%20Klein%20Burst%20Analog%20Women%27s%20Generic%20%0ASKU%3A%20Ck%0APrice%3A%20%E2%82%B91499%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
