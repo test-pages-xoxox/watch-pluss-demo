db892962386630ae40f468e559d1c0157feebf22
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-16 20:06:16 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -1934,5 +1934,31 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20FS5406%20Formal%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20FS5406%20Formal%20Generic%20%0ASKU%3A%20Fossil%0APrice%3A%20%E2%82%B91599%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Fossil Bronson Automatic Generic ",
+        "slug": "fossil-bronson-automatic-generic-af989284cf08",
+        "sku": "Fossl",
+        "brand": "Fossil generic ",
+        "badge": "Hot",
+        "availability": "In stock ",
+        "description": "Fossil\r\n# For Men\r\n# 7AA Premium Collection\r\n# Model - Fossil Bronson Automatic \r\n# Dial Size - 44mm\r\n# Features following -",
+        "details": "",
+        "tags": "fossil,formal,chain",
+        "price": {
+            "old": "₹3200",
+            "new": "₹2100"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-bronson-automatic-generic-af989284cf08/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-bronson-automatic-generic-af989284cf08/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-bronson-automatic-generic-af989284cf08/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-bronson-automatic-generic-af989284cf08/other2.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Bronson%20Automatic%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Bronson%20Automatic%20Generic%20%0ASKU%3A%20Fossl%0APrice%3A%20%E2%82%B92100%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
