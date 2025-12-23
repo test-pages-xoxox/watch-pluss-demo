6788bc4038cfbfe8c7dbbe1b94a1ebdc54a2e2c4
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-23 16:25:40 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2418,5 +2418,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20GMT%20Master%202%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20GMT%20Master%202%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Rolex Daytona  Generic ",
+        "slug": "rolex-daytona-generic-1f6960343721",
+        "sku": "Rx",
+        "brand": "Rolex ",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "*Rolex Daytona Cosmograph Original Model Series Now Available* ",
+        "details": "",
+        "tags": "rolex,formal,men",
+        "price": {
+            "old": "₹4500",
+            "new": "₹2600"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-daytona-generic-1f6960343721/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-daytona-generic-1f6960343721/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-daytona-generic-1f6960343721/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Daytona%20%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Daytona%20%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
