11908945d92a0abc4e0ba0bdb78ff33b38f711da
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-23 17:35:12 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2544,5 +2544,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Datejust%2026mm%20generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Datejust%2026mm%20generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92300%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Rolex Datejust 36mm Generic ",
+        "slug": "rolex-datejust-36mm-generic-26b99a6b0f5d",
+        "sku": "Rx",
+        "brand": "Rolex ",
+        "badge": "Hot",
+        "availability": "In stock ",
+        "description": "",
+        "details": "",
+        "tags": "rolex,formal,chain",
+        "price": {
+            "old": "₹4300",
+            "new": "₹2600"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-36mm-generic-26b99a6b0f5d/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-36mm-generic-26b99a6b0f5d/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-36mm-generic-26b99a6b0f5d/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Datejust%2036mm%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Datejust%2036mm%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
