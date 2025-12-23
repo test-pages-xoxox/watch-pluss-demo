03b1038b16dae8661e25c26553c8eb2dd9a50356
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-23 15:33:19 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2291,5 +2291,31 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Bvlgari%20Serpenti%20Women%27s%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Bvlgari%20Serpenti%20Women%27s%20Generic%20%0ASKU%3A%20bvlgari%0APrice%3A%20%E2%82%B92299%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Fossil Jacqueline Collection Generic ",
+        "slug": "fossil-jacqueline-collection-generic-ce27f9753529",
+        "sku": "Fossil ",
+        "brand": "Fossil generic ",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "Fossil\r\n# For Her \r\n# 7AA Ultra Premium Series\r\n# Model - Jacqueline Collection\r\n# Dial Size - 36mm",
+        "details": "",
+        "tags": "women-chain, women-formal, ",
+        "price": {
+            "old": "₹3200",
+            "new": "₹2600"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-jacqueline-collection-generic-ce27f9753529/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-jacqueline-collection-generic-ce27f9753529/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-jacqueline-collection-generic-ce27f9753529/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-jacqueline-collection-generic-ce27f9753529/other2.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Jacqueline%20Collection%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Jacqueline%20Collection%20Generic%20%0ASKU%3A%20Fossil%20%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
