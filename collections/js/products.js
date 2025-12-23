24be27e09e17dae25331dcc139bd63d3fd945608
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-23 16:42:38 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2472,27 +2472,27 @@ const products = [
     },
     {
         "id": "#",
-        "name": "Rolex Blue Generic ",
-        "slug": "rolex-blue-generic-3d88575ae016",
+        "name": "Rolex GMT Master Generic ",
+        "slug": "rolex-gmt-master-generic-1ea3ee4c3f5e",
         "sku": "Rx",
         "brand": "Rolex ",
-        "badge": "Hot ",
+        "badge": "Hot",
         "availability": "In stock ",
         "description": "",
         "details": "",
         "tags": "rolex,formal,chain",
         "price": {
-            "old": "₹4600",
-            "new": "₹2600"
+            "old": "₹5800",
+            "new": "₹2700"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-blue-generic-3d88575ae016/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-blue-generic-3d88575ae016/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-gmt-master-generic-1ea3ee4c3f5e/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-gmt-master-generic-1ea3ee4c3f5e/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-blue-generic-3d88575ae016/other1.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-gmt-master-generic-1ea3ee4c3f5e/other1.webp"
             ]
         },
-        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Blue%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Blue%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20GMT%20Master%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20GMT%20Master%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
