46af4985bc3a8c5c2854adc0b8c3fd8515be822d
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-23 17:40:06 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2547,27 +2547,27 @@ const products = [
     },
     {
         "id": "#",
-        "name": "Rolex GMT-Master 40mm  Generic ",
-        "slug": "rolex-gmt-master-40mm-generic-f1f0d7c99e2e",
+        "name": "Rolex Submariner No Date 41mm Generic ",
+        "slug": "rolex-submariner-no-date-41mm-generic-6dce19a5392f",
         "sku": "Rx",
         "brand": "Rolex ",
-        "badge": "Hot",
+        "badge": "Hot ",
         "availability": "In stock ",
         "description": "",
         "details": "",
         "tags": "rolex,formal,chain",
         "price": {
-            "old": "₹4700",
+            "old": "₹4600",
             "new": "₹2700"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-gmt-master-40mm-generic-f1f0d7c99e2e/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-gmt-master-40mm-generic-f1f0d7c99e2e/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-submariner-no-date-41mm-generic-6dce19a5392f/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-submariner-no-date-41mm-generic-6dce19a5392f/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-gmt-master-40mm-generic-f1f0d7c99e2e/other1.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-submariner-no-date-41mm-generic-6dce19a5392f/other1.webp"
             ]
         },
-        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20GMT-Master%2040mm%20%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20GMT-Master%2040mm%20%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Submariner%20No%20Date%2041mm%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Submariner%20No%20Date%2041mm%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
