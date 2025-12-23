07539304f8c00f27608f3ab181e36dc353057858
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-23 17:36:53 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2548,10 +2548,10 @@ const products = [
     {
         "id": "#",
         "name": "Rolex Datejust 36mm Generic ",
-        "slug": "rolex-datejust-36mm-generic-26b99a6b0f5d",
+        "slug": "rolex-datejust-36mm-generic-0f8877dd4315",
         "sku": "Rx",
         "brand": "Rolex ",
-        "badge": "Hot",
+        "badge": "Hot ",
         "availability": "In stock ",
         "description": "",
         "details": "",
@@ -2561,10 +2561,10 @@ const products = [
             "new": "₹2600"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-36mm-generic-26b99a6b0f5d/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-36mm-generic-26b99a6b0f5d/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-36mm-generic-0f8877dd4315/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-36mm-generic-0f8877dd4315/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-36mm-generic-26b99a6b0f5d/other1.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-36mm-generic-0f8877dd4315/other1.webp"
             ]
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Datejust%2036mm%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
