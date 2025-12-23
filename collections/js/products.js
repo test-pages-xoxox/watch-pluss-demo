6acb127da6910a9f4635c6e26dfc1745d0204bb2
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-23 15:51:26 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2317,5 +2317,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Jacqueline%20Collection%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Jacqueline%20Collection%20Generic%20%0ASKU%3A%20Fossil%20%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Tommy Hilfiger Clark Chronograph Series Generic ",
+        "slug": "tommy-hilfiger-clark-chronograph-series-generic-1b9926222f98",
+        "sku": "Tom",
+        "brand": "Tommy Hilfiger",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "Brand:\tTommy Hilfiger\r\nFor: Him\r\nQuality: 7AA Premium Collection\r\nModel: Clark Chronograph Series",
+        "details": "",
+        "tags": "tommy,formal,men",
+        "price": {
+            "old": "₹2500",
+            "new": "₹1749"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-clark-chronograph-series-generic-1b9926222f98/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-clark-chronograph-series-generic-1b9926222f98/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-clark-chronograph-series-generic-1b9926222f98/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tommy%20Hilfiger%20Clark%20Chronograph%20Series%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tommy%20Hilfiger%20Clark%20Chronograph%20Series%20Generic%20%0ASKU%3A%20Tom%0APrice%3A%20%E2%82%B91749%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
