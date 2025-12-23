5ee0e5caa5f9d91807ee0d976527066b256ebba4
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-23 17:48:00 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2573,7 +2573,7 @@ const products = [
     {
         "id": "#",
         "name": "Rolex Datejust 41mm Generic ",
-        "slug": "rolex-datejust-41mm-generic-00d8c2f03569",
+        "slug": "rolex-datejust-41mm-generic-d173f2bf5605",
         "sku": "Rx",
         "brand": "Rolex ",
         "badge": "Hot ",
@@ -2583,16 +2583,16 @@ const products = [
         "tags": "rolex,formal,chain",
         "price": {
             "old": "₹4700",
-            "new": "₹2600"
+            "new": "₹2500"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-41mm-generic-00d8c2f03569/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-41mm-generic-00d8c2f03569/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-41mm-generic-d173f2bf5605/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-41mm-generic-d173f2bf5605/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-41mm-generic-00d8c2f03569/other1.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-datejust-41mm-generic-d173f2bf5605/other1.webp"
             ]
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Datejust%2041mm%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Datejust%2041mm%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Datejust%2041mm%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92500%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
