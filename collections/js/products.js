49d207eef2739a056491c04f09dfc0ef834d00ae
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-23 18:02:02 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2644,5 +2644,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Oyster%20Perpetual%2041mm%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Oyster%20Perpetual%2041mm%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Rolex GMT Master 40mm Generic ",
+        "slug": "rolex-gmt-master-40mm-generic-0c7ce1e228ac",
+        "sku": "Rx",
+        "brand": "Rolex ",
+        "badge": "Hot",
+        "availability": "In stock ",
+        "description": "",
+        "details": "",
+        "tags": "rolex,formal,chain",
+        "price": {
+            "old": "₹3600",
+            "new": "₹2600"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-gmt-master-40mm-generic-0c7ce1e228ac/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-gmt-master-40mm-generic-0c7ce1e228ac/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-gmt-master-40mm-generic-0c7ce1e228ac/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20GMT%20Master%2040mm%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20GMT%20Master%2040mm%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
