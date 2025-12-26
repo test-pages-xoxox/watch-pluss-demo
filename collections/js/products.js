8b04fc581d557db7bf1bfc5345f097b7f8f5de2d
test-pages-xoxox <umer.qureshi.work@gmail.com>
2025-12-26 16:23:32 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2850,5 +2850,31 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tissot%20Vuelta%20Chronograph%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tissot%20Vuelta%20Chronograph%20Generic%20%0ASKU%3A%20Tiso%0APrice%3A%20%E2%82%B91800%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Fossil Neutra Chronograph Generic ",
+        "slug": "fossil-neutra-chronograph-generic-1a2accb142c0",
+        "sku": "Fossil ",
+        "brand": "Fossil ",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "Fossil\r\n# For Men\r\n# 7AA Premium Quality Collection\r\n# Model - Neutra Chronograph\r\n# Model No. - FS5512\r\n# Dial Size - 43mm",
+        "details": "",
+        "tags": "fossil,formal,leather",
+        "price": {
+            "old": "₹2900",
+            "new": "₹1799"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-neutra-chronograph-generic-1a2accb142c0/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-neutra-chronograph-generic-1a2accb142c0/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-neutra-chronograph-generic-1a2accb142c0/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-neutra-chronograph-generic-1a2accb142c0/other2.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Neutra%20Chronograph%20Generic%20.%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Neutra%20Chronograph%20Generic%20%0ASKU%3A%20Fossil%20%0APrice%3A%20%E2%82%B91799%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
