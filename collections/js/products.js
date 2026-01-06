132c18bc6b40b9ca6f0b3354ecb388f1f011befd
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-06 14:28:47 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -2978,5 +2978,31 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Automatic%20Machine%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dfossil-automatic-machine-generic-9fc607875d06%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Automatic%20Machine%20Generic%20%0ASKU%3A%20Fossil%20%0APrice%3A%20%E2%82%B92200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Tissot Class Automatic Women's Generic ",
+        "slug": "tissot-class-automatic-women-s-generic-053e848cf0cf",
+        "sku": "Tissot ",
+        "brand": "Tissot ",
+        "badge": "Hot",
+        "availability": "In stock ",
+        "description": "*Tissot T-Lady Class Automatic Open Back Collection detailed with Perfection* \r\n*“Unveiling elegance that never fades — Tissot T-Classic Women’s Ultra-Premium watch.”* 🌟",
+        "details": "",
+        "tags": "women-chain, women-formal, ",
+        "price": {
+            "old": "₹3600",
+            "new": "₹2100"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-class-automatic-women-s-generic-053e848cf0cf/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-class-automatic-women-s-generic-053e848cf0cf/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-class-automatic-women-s-generic-053e848cf0cf/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-class-automatic-women-s-generic-053e848cf0cf/other2.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tissot%20Class%20Automatic%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dtissot-class-automatic-women-s-generic-053e848cf0cf%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tissot%20Class%20Automatic%20Women%27s%20Generic%20%0ASKU%3A%20Tissot%20%0APrice%3A%20%E2%82%B92100%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
