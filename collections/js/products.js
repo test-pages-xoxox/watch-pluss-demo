7ae1aa961fb6a8cfa2cc646b1f4716b6ff235493
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-06 14:37:55 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3006,27 +3006,28 @@ const products = [
     },
     {
         "id": "#",
-        "name": "Bvlgari Serpenti Women's Generic ",
-        "slug": "bvlgari-serpenti-women-s-generic-3ab2ee8b00c4",
-        "sku": "Bvlgari",
-        "brand": "Bvlgari Serpenti ",
+        "name": "Burberry Women's Generic ",
+        "slug": "burberry-women-s-generic-517762f9c465",
+        "sku": "Burberry",
+        "brand": "Burberry",
         "badge": "Hot",
         "availability": "In stock ",
-        "description": "*“Bvlgari Serpenti in Rose Gold — for those who dare to shine differently.”* ",
+        "description": "*An icon of style that defines every moment — Burberry elegance unveiled.* ",
         "details": "",
-        "tags": "women-chain, women-formal,women-bvlgari",
+        "tags": "women-chain, women-formal, ",
         "price": {
-            "old": "₹3800",
-            "new": "₹2300"
+            "old": "₹2800",
+            "new": "₹1600"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/bvlgari-serpenti-women-s-generic-3ab2ee8b00c4/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/bvlgari-serpenti-women-s-generic-3ab2ee8b00c4/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/burberry-women-s-generic-517762f9c465/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/burberry-women-s-generic-517762f9c465/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/bvlgari-serpenti-women-s-generic-3ab2ee8b00c4/other1.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/burberry-women-s-generic-517762f9c465/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/burberry-women-s-generic-517762f9c465/other2.webp"
             ]
         },
-        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Bvlgari%20Serpenti%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dbvlgari-serpenti-women-s-generic-3ab2ee8b00c4%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Bvlgari%20Serpenti%20Women%27s%20Generic%20%0ASKU%3A%20Bvlgari%0APrice%3A%20%E2%82%B92300%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Burberry%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dburberry-women-s-generic-517762f9c465%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Burberry%20Women%27s%20Generic%20%0ASKU%3A%20Burberry%0APrice%3A%20%E2%82%B91600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
