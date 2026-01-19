75aba3dbe82acd820d47b9ccad59ebf6401037f1
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-19 17:45:20 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3359,5 +3359,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Christian%20Dior%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dchristian-dior-women-s-generic-14eb8cfa9d35%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Christian%20Dior%20Women%27s%20Generic%20%0ASKU%3A%20Dior%20%0APrice%3A%20%E2%82%B91300%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Gucci Women's Generic ",
+        "slug": "gucci-women-s-generic-9eab5b236a6e",
+        "sku": "Gucci",
+        "brand": "Gucci",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "Gucci\r\n# For Her\r\n# 7AA Premium Collection \r\n# Patent Dial Series \r\n# Dial Size - 32mm\r\n# Features follows - \r\n\r\n*EXPRESS YOUR LOVE WITH THIS BEAUTIFUL TIMEPIECE",
+        "details": "",
+        "tags": "women-chain, women-formal, ",
+        "price": {
+            "old": "₹2300",
+            "new": "₹1400"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/gucci-women-s-generic-9eab5b236a6e/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/gucci-women-s-generic-9eab5b236a6e/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/gucci-women-s-generic-9eab5b236a6e/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Gucci%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dgucci-women-s-generic-9eab5b236a6e%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Gucci%20Women%27s%20Generic%20%0ASKU%3A%20Gucci%0APrice%3A%20%E2%82%B91400%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
