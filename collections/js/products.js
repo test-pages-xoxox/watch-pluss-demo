a5a4b97565c82c91897a3956d40746070ab31be3
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-19 17:54:41 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3387,27 +3387,27 @@ const products = [
     },
     {
         "id": "#",
-        "name": "Audemars Piguet Generic ",
-        "slug": "audemars-piguet-generic-db84c8f20112",
-        "sku": "Ap",
-        "brand": "Audemars Piguet",
-        "badge": "Hot ",
+        "name": "Gucci Sunglasses ",
+        "slug": "gucci-sunglasses-af122a502fbe",
+        "sku": "Gucci ",
+        "brand": "Gucci ",
+        "badge": "Trending ",
         "availability": "In stock ",
-        "description": "Brand - Audemars Piguet\r\n# For - Him\r\n# Quality - 7AA Premium Collection \r\n# Model - Royal Oak Tourbillon\r\n# Case Size - 42mm",
+        "description": "New arrivals\r\nGucci \r\nUnisex \r\nFiber ",
         "details": "",
-        "tags": "audemars,formal,men",
+        "tags": "sunglasses",
         "price": {
-            "old": "₹5400",
-            "new": "₹3200"
+            "old": "₹1999",
+            "new": "₹950"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/audemars-piguet-generic-db84c8f20112/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/audemars-piguet-generic-db84c8f20112/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/gucci-sunglasses-af122a502fbe/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/gucci-sunglasses-af122a502fbe/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/audemars-piguet-generic-db84c8f20112/other1.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/gucci-sunglasses-af122a502fbe/other1.webp"
             ]
         },
-        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Audemars%20Piguet%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Daudemars-piguet-generic-db84c8f20112%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Audemars%20Piguet%20Generic%20%0ASKU%3A%20Ap%0APrice%3A%20%E2%82%B93200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Gucci%20Sunglasses%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dgucci-sunglasses-af122a502fbe%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Gucci%20Sunglasses%20%0ASKU%3A%20Gucci%20%0APrice%3A%20%E2%82%B9950%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
