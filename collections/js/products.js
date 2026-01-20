dc08412412e26532c66658e24d4f2e8a113de3d7
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-21 00:54:05 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3561,5 +3561,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rado%20Couple%20Collection%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Drado-couple-collection-generic-fef71bf8347c%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rado%20Couple%20Collection%20Generic%20%0ASKU%3A%20Rado%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Rado Couple Collection Generic ",
+        "slug": "rado-couple-collection-generic-75c97d64b955",
+        "sku": "Rado",
+        "brand": "rado",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "Couole watch ⌚️ \r\nGood quality \r\nDate.  Working \r\nBest price updates",
+        "details": "",
+        "tags": "couple",
+        "price": {
+            "old": "₹3800",
+            "new": "₹2600"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rado-couple-collection-generic-75c97d64b955/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rado-couple-collection-generic-75c97d64b955/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rado-couple-collection-generic-75c97d64b955/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rado%20Couple%20Collection%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Drado-couple-collection-generic-75c97d64b955%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rado%20Couple%20Collection%20Generic%20%0ASKU%3A%20Rado%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
