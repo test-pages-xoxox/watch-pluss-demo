ec0b5b484e9e3deccc6881a573e3dfe74e60a615
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-21 11:42:58 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3639,11 +3639,11 @@ const products = [
     },
     {
         "id": "#",
-        "name": "Prada  unisex Fiber Sunglasses Generic ",
-        "slug": "prada-unisex-fiber-sunglasses-generic-eb392ec40a01",
-        "sku": "Pra",
-        "brand": "Prada",
-        "badge": "Hot ",
+        "name": "Versace  unisex Fiber Sunglasses Generic ",
+        "slug": "versace-unisex-fiber-sunglasses-generic-9ac9dd1bf6bc",
+        "sku": "Ver",
+        "brand": "Versace ",
+        "badge": "Trending ",
         "availability": "In stock ",
         "description": "",
         "details": "",
@@ -3653,13 +3653,14 @@ const products = [
             "new": "₹950"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/prada-unisex-fiber-sunglasses-generic-eb392ec40a01/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/prada-unisex-fiber-sunglasses-generic-eb392ec40a01/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/versace-unisex-fiber-sunglasses-generic-9ac9dd1bf6bc/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/versace-unisex-fiber-sunglasses-generic-9ac9dd1bf6bc/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/prada-unisex-fiber-sunglasses-generic-eb392ec40a01/other1.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/versace-unisex-fiber-sunglasses-generic-9ac9dd1bf6bc/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/versace-unisex-fiber-sunglasses-generic-9ac9dd1bf6bc/other2.webp"
             ]
         },
-        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Prada%20%20unisex%20Fiber%20Sunglasses%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dprada-unisex-fiber-sunglasses-generic-eb392ec40a01%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Prada%20%20unisex%20Fiber%20Sunglasses%20Generic%20%0ASKU%3A%20Pra%0APrice%3A%20%E2%82%B9950%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Versace%20%20unisex%20Fiber%20Sunglasses%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dversace-unisex-fiber-sunglasses-generic-9ac9dd1bf6bc%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Versace%20%20unisex%20Fiber%20Sunglasses%20Generic%20%0ASKU%3A%20Ver%0APrice%3A%20%E2%82%B9950%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
