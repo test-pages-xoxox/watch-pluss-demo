c0889e5587b902e1ebb7b69594330f8244d962a0
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-21 11:28:14 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3611,5 +3611,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rado%20Gold%20men%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Drado-gold-men-s-generic-9e13f4178e80%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rado%20Gold%20men%27s%20Generic%20%0ASKU%3A%20Rado%0APrice%3A%20%E2%82%B93200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Tissot PRX Automatic 80 Generic ",
+        "slug": "tissot-prx-automatic-80-generic-fa03ca1dd6a5",
+        "sku": "Tissot ",
+        "brand": "Tissot ",
+        "badge": "Hot",
+        "availability": "In stock ",
+        "description": "Tissot\r\n# For Men\r\n# 7AA Ultra Premium collection\r\n# Model - PRX Powermatic 80\r\n# Dial Size - 40mm\r\n# Features following -",
+        "details": "",
+        "tags": "tissot,formal,chain,men",
+        "price": {
+            "old": "₹4700",
+            "new": "₹3200"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-prx-automatic-80-generic-fa03ca1dd6a5/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-prx-automatic-80-generic-fa03ca1dd6a5/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-prx-automatic-80-generic-fa03ca1dd6a5/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tissot%20PRX%20Automatic%2080%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dtissot-prx-automatic-80-generic-fa03ca1dd6a5%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tissot%20PRX%20Automatic%2080%20Generic%20%0ASKU%3A%20Tissot%20%0APrice%3A%20%E2%82%B93200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
