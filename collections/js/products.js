fca8cf9347fe4cd0dee2a2629b4480f06b3a3bb1
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-21 16:43:49 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3713,5 +3713,32 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Armani%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Darmani-women-s-generic-f589610a3c65%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Armani%20Women%27s%20Generic%20%0ASKU%3A%20Armani%20%0APrice%3A%20%E2%82%B91800%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Fossil Leather Women's Generic ",
+        "slug": "fossil-leather-women-s-generic-cfb90e8bb400",
+        "sku": "Fos",
+        "brand": "Fossil ",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "",
+        "details": "",
+        "tags": "women-fossil,formal,leather,women",
+        "price": {
+            "old": "₹2300",
+            "new": "₹1500"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-women-s-generic-cfb90e8bb400/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-women-s-generic-cfb90e8bb400/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-women-s-generic-cfb90e8bb400/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-women-s-generic-cfb90e8bb400/other2.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-women-s-generic-cfb90e8bb400/other3.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Leather%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dfossil-leather-women-s-generic-cfb90e8bb400%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Leather%20Women%27s%20Generic%20%0ASKU%3A%20Fos%0APrice%3A%20%E2%82%B91500%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
