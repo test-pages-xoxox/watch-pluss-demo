44ec823399bc90384e4e067b4bd06ce00039a395
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-21 11:25:48 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3589,25 +3589,27 @@ const products = [
     },
     {
         "id": "#",
-        "name": "SWAROVSKI Women's Generic ",
-        "slug": "swarovski-women-s-generic-d77dcbc23ce7",
-        "sku": "Sk",
-        "brand": "SWAROVSKI",
-        "badge": "Hot",
+        "name": "Rado Gold men's Generic ",
+        "slug": "rado-gold-men-s-generic-9e13f4178e80",
+        "sku": "Rado",
+        "brand": "rado",
+        "badge": "Hot ",
         "availability": "In stock ",
-        "description": "Matrix Bangle\r\nCase size: 25 mm x 25 mm\r\nWatch strap length: 16.5 cm\r\nMaterial:  Crystals, Rose gold-tone PVD\r\nColor:  Gray,Pink,Blue,White, S-White\r\nClasp type:  Fold over clasp\r\nStrap material:  Crystals\r\nMechanism:  Quartz",
+        "description": "Rado\r\n# For Men\r\n# 7AA Premium Only\r\n# Diastar New Original Chronograph Model 💯 \r\n# Dial Size - 44mm\r\n# Features Following -",
         "details": "",
-        "tags": "women-chain, women-formal,women",
+        "tags": "rado,formal,chain,men",
         "price": {
-            "old": "₹3800",
-            "new": "₹2100"
+            "old": "₹4700",
+            "new": "₹3200"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/swarovski-women-s-generic-d77dcbc23ce7/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/swarovski-women-s-generic-d77dcbc23ce7/hover.webp",
-            "others": []
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rado-gold-men-s-generic-9e13f4178e80/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rado-gold-men-s-generic-9e13f4178e80/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rado-gold-men-s-generic-9e13f4178e80/other1.webp"
+            ]
         },
-        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20SWAROVSKI%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dswarovski-women-s-generic-d77dcbc23ce7%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20SWAROVSKI%20Women%27s%20Generic%20%0ASKU%3A%20Sk%0APrice%3A%20%E2%82%B92100%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rado%20Gold%20men%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Drado-gold-men-s-generic-9e13f4178e80%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rado%20Gold%20men%27s%20Generic%20%0ASKU%3A%20Rado%0APrice%3A%20%E2%82%B93200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
