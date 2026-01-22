4de25c88d109fec7899ca36fab3aa8e648698bce
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-22 11:47:30 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3816,5 +3816,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Automatic%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dfossil-automatic-generic-d45fff7f5882%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Automatic%20Generic%20%0ASKU%3A%20Fossil%20%0APrice%3A%20%E2%82%B92200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Fossil Mens Generic ",
+        "slug": "fossil-mens-generic-8cf3d2f25f84",
+        "sku": "Fossil ",
+        "brand": "Fossil ",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "Working chronograph \r\n✅Stainless stell case\r\n✅Full black case\r\n✅Black dial\r\n✅Date indicator\r\n✅Strap material-Stainless steel\r\n✅High end quality battery operated machine \r\n",
+        "details": "",
+        "tags": "fossil,formal,chain,men",
+        "price": {
+            "old": "₹3200",
+            "new": "₹1700"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-mens-generic-8cf3d2f25f84/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-mens-generic-8cf3d2f25f84/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-mens-generic-8cf3d2f25f84/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Mens%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dfossil-mens-generic-8cf3d2f25f84%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Mens%20Generic%20%0ASKU%3A%20Fossil%20%0APrice%3A%20%E2%82%B91700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
