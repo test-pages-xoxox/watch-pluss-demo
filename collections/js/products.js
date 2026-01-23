b42b1918b7846b5fe7720fd1989a931dd467e7d4
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-23 19:26:25 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -3971,5 +3971,34 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Armani%20exchange%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Darmani-exchange-generic-ca2e64101807%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Armani%20exchange%20Generic%20%0ASKU%3A%20Armani%20%0APrice%3A%20%E2%82%B91700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Fossil Leather Generic ",
+        "slug": "fossil-leather-generic-1d35703285f9",
+        "sku": "Fossil ",
+        "brand": "Fossil ",
+        "badge": "Hot",
+        "availability": "In stock ",
+        "description": "Premium Craftsmanship & Elegant Design ✨\r\n\t•\t✅ Silver gold rose gold blck Stainless Steel Bracelet – A Classy Statement\r\n\t•\t\r\n\t•\t✅ Fully Functional Chronograph with 24-Hour Display\r\n\t•\t✅ Sleek Gold Bezel & Durable Scratch-Resistant Glass\r\n\t•\t✅ Water & Splash Resistant – Built for Everyday Elegance",
+        "details": "",
+        "tags": "fossil,formal,leather,men",
+        "price": {
+            "old": "₹2300",
+            "new": "₹1700"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-generic-1d35703285f9/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-generic-1d35703285f9/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-generic-1d35703285f9/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-generic-1d35703285f9/other2.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-generic-1d35703285f9/other3.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-generic-1d35703285f9/other4.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/fossil-leather-generic-1d35703285f9/other5.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Leather%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dfossil-leather-generic-1d35703285f9%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Leather%20Generic%20%0ASKU%3A%20Fossil%20%0APrice%3A%20%E2%82%B91700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
