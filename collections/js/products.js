1c3ea5d354810a7fe6587767658b790592532057
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-27 17:59:04 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4172,5 +4172,32 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Cartier%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dcartier-women-s-generic-3d60a3accfd5%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Cartier%20Women%27s%20Generic%20%0ASKU%3A%20Cr%0APrice%3A%20%E2%82%B91300%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Maybach  Unisex metal fiber Generic ",
+        "slug": "maybach-unisex-metal-fiber-generic-f63349f8e9f1",
+        "sku": "Sun",
+        "brand": "Maybach ",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "Maybach \r\nUnisex\r\nmetal fiber",
+        "details": "",
+        "tags": "sunglasses",
+        "price": {
+            "old": "₹1999",
+            "new": "₹950"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/maybach-unisex-metal-fiber-generic-f63349f8e9f1/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/maybach-unisex-metal-fiber-generic-f63349f8e9f1/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/maybach-unisex-metal-fiber-generic-f63349f8e9f1/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/maybach-unisex-metal-fiber-generic-f63349f8e9f1/other2.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/maybach-unisex-metal-fiber-generic-f63349f8e9f1/other3.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Maybach%20%20Unisex%20metal%20fiber%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dmaybach-unisex-metal-fiber-generic-f63349f8e9f1%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Maybach%20%20Unisex%20metal%20fiber%20Generic%20%0ASKU%3A%20Sun%0APrice%3A%20%E2%82%B9950%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
