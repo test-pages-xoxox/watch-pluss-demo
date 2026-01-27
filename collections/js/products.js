41bdff0ff960fd18cde832bdff8448a5c84f8416
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-27 15:02:19 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4077,28 +4077,27 @@ const products = [
     },
     {
         "id": "#",
-        "name": "Rolex Generic ",
-        "slug": "rolex-generic-39b6d9deb09d",
-        "sku": "Rx",
-        "brand": "Rolex ",
+        "name": "Tissot collection Generic ",
+        "slug": "tissot-collection-generic-553bfac5ea66",
+        "sku": "Tissot ",
+        "brand": "Tissot ",
         "badge": "Hot",
         "availability": "In stock ",
-        "description": "Rolex\r\n✅For Men\r\n✅ 7AA Premium Collection\r\n✅ Model - Rolex Geneve\r\n✅Dial Size - 43mm (Original Size)",
+        "description": "Brand -Tissot \r\n✅Movement-Automatic\r\n✅Size case-42mm\r\n✅Case material-Stainless steel\r\n✅Sapphire crystal glass\r\n✅Black dial colour \r\n✅stainless metal strap\r\n✅*AUTOMATIC MACHINE WITH 1 YEAR WARRANTY",
         "details": "",
-        "tags": "rolex,formal,chain,men",
+        "tags": "tissot,formal,chain,men",
         "price": {
-            "old": "₹3800",
-            "new": "₹2300"
+            "old": "₹4700",
+            "new": "₹1800"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-generic-39b6d9deb09d/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-generic-39b6d9deb09d/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-collection-generic-553bfac5ea66/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-collection-generic-553bfac5ea66/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-generic-39b6d9deb09d/other1.webp",
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rolex-generic-39b6d9deb09d/other2.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tissot-collection-generic-553bfac5ea66/other1.webp"
             ]
         },
-        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Drolex-generic-39b6d9deb09d%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Generic%20%0ASKU%3A%20Rx%0APrice%3A%20%E2%82%B92300%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tissot%20collection%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dtissot-collection-generic-553bfac5ea66%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tissot%20collection%20Generic%20%0ASKU%3A%20Tissot%20%0APrice%3A%20%E2%82%B91800%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
