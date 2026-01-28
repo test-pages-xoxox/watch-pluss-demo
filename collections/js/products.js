cfa165ac99d16f5212d405fc16f75671d3d99d4f
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-28 10:59:20 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4228,5 +4228,36 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Guess%20Women%27s%20collection%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dguess-women-s-collection-generic-617f738f9523%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Guess%20Women%27s%20collection%20Generic%20%0ASKU%3A%20Guess%20%0APrice%3A%20%E2%82%B91500%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Calvin Klein Women's Generic ",
+        "slug": "calvin-klein-women-s-generic-a60b30e4299b",
+        "sku": "Ck",
+        "brand": "Calvin Klein",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "• 7A Premium Quality And Enchant Model For Ladies\r\n• Diamond Cut Glass Dial Case, Swiss Made And Quartz Movement\r\n• Stainless Steel Material, Premium Quality Metal Belt And Close Buckle Lock \r\n• Calvin Klein Is The Top Of Materials For Watches That Are Designed To Stand The Test Of Timing\r\n➡️ Available Stock & Ready To Book Now\r\n➡️ Same Day Dispatch & COD Pickup Available\r\n➡️ Product Will Be Delivered Same As In Picture",
+        "details": "",
+        "tags": "women-chain, women-formal,women",
+        "price": {
+            "old": "₹2300",
+            "new": "₹1300"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-women-s-generic-a60b30e4299b/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-women-s-generic-a60b30e4299b/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-women-s-generic-a60b30e4299b/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-women-s-generic-a60b30e4299b/other2.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-women-s-generic-a60b30e4299b/other3.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-women-s-generic-a60b30e4299b/other4.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-women-s-generic-a60b30e4299b/other5.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-women-s-generic-a60b30e4299b/other6.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/calvin-klein-women-s-generic-a60b30e4299b/other7.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Calvin%20Klein%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dcalvin-klein-women-s-generic-a60b30e4299b%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Calvin%20Klein%20Women%27s%20Generic%20%0ASKU%3A%20Ck%0APrice%3A%20%E2%82%B91300%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
