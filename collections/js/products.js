78106ad048a50f39d5212ff641489ecf87e89e31
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-01-29 21:01:08 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4308,10 +4308,10 @@ const products = [
     },
     {
         "id": "#",
-        "name": "Police  Fiber  Unisex Generic ",
-        "slug": "police-fiber-unisex-generic-585ffeb5bf1f",
+        "name": "Adidas  Polorised  Fiber  Unisex Generic ",
+        "slug": "adidas-polorised-fiber-unisex-generic-92c9c9fd0084",
         "sku": "Sun",
-        "brand": "Police ",
+        "brand": "Adidas",
         "badge": "Trending ",
         "availability": "In stock ",
         "description": "",
@@ -4322,17 +4322,14 @@ const products = [
             "new": "₹950"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/police-fiber-unisex-generic-585ffeb5bf1f/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/police-fiber-unisex-generic-585ffeb5bf1f/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/adidas-polorised-fiber-unisex-generic-92c9c9fd0084/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/adidas-polorised-fiber-unisex-generic-92c9c9fd0084/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/police-fiber-unisex-generic-585ffeb5bf1f/other1.webp",
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/police-fiber-unisex-generic-585ffeb5bf1f/other2.webp",
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/police-fiber-unisex-generic-585ffeb5bf1f/other3.webp",
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/police-fiber-unisex-generic-585ffeb5bf1f/other4.webp",
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/police-fiber-unisex-generic-585ffeb5bf1f/other5.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/adidas-polorised-fiber-unisex-generic-92c9c9fd0084/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/adidas-polorised-fiber-unisex-generic-92c9c9fd0084/other2.webp"
             ]
         },
-        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Police%20%20Fiber%20%20Unisex%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dpolice-fiber-unisex-generic-585ffeb5bf1f%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Police%20%20Fiber%20%20Unisex%20Generic%20%0ASKU%3A%20Sun%0APrice%3A%20%E2%82%B9950%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Adidas%20%20Polorised%20%20Fiber%20%20Unisex%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dadidas-polorised-fiber-unisex-generic-92c9c9fd0084%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Adidas%20%20Polorised%20%20Fiber%20%20Unisex%20Generic%20%0ASKU%3A%20Sun%0APrice%3A%20%E2%82%B9950%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
