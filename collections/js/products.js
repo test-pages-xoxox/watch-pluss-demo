66ad6d8b6b4b8dc6a282cffdc2dd084d39dbcc03
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-03 18:06:13 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4407,5 +4407,31 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tommy%20Hilfiger%20%20TH1710401%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dtommy-hilfiger-th1710401-generic-ee1a2c1d369b%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tommy%20Hilfiger%20%20TH1710401%20Generic%20%0ASKU%3A%20Tom%0APrice%3A%20%E2%82%B91750%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Cartier TANK LOUIS CARTIER WATCH Generic ",
+        "slug": "cartier-tank-louis-cartier-watch-generic-5bff3455f7b5",
+        "sku": "Cr",
+        "brand": "Cartier",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "Working 2 Hand Face\r\n- Case in Rose-Gold \r\n- Beaded Crown Set With A Sapphire Cabochon\r\n- White Beaded Dial\r\n- Blued-Steel Sword-Shaped Hands\r\n- Strap In Semi-Matt Brown Alligator Skin\r\n- *100% reliable and trusted battery operated Machinery",
+        "details": "",
+        "tags": "men,formal,leather",
+        "price": {
+            "old": "₹3200",
+            "new": "₹1800"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/cartier-tank-louis-cartier-watch-generic-5bff3455f7b5/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/cartier-tank-louis-cartier-watch-generic-5bff3455f7b5/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/cartier-tank-louis-cartier-watch-generic-5bff3455f7b5/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/cartier-tank-louis-cartier-watch-generic-5bff3455f7b5/other2.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Cartier%20TANK%20LOUIS%20CARTIER%20WATCH%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dcartier-tank-louis-cartier-watch-generic-5bff3455f7b5%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Cartier%20TANK%20LOUIS%20CARTIER%20WATCH%20Generic%20%0ASKU%3A%20Cr%0APrice%3A%20%E2%82%B91800%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
