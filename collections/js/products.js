b852a65bd6a3674704d33f9411b359fbd99875f6
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-07 17:57:12 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4505,5 +4505,31 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Versace%20Women%27s%20Rose%20Gold%20Collection%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dversace-women-s-rose-gold-collection-generic-6031b6d8b0c6%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Versace%20Women%27s%20Rose%20Gold%20Collection%20Generic%20%0ASKU%3A%20Versace%20%0APrice%3A%20%E2%82%B91700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Tommy Hilfiger Modern Automatic Generic ",
+        "slug": "tommy-hilfiger-modern-automatic-generic-a013abece7c4",
+        "sku": "Tom",
+        "brand": "Tommy Hilfiger",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "- 3 Hands Skeleton face\r\n- Brand Detailing in the Open Heart\r\n- Occasion: Luxury, Formal, Party Wear\r\n- Glass: Mineral Crystal\r\n- Strap / Bracelet: Stainless Steel Gold Bracelet\r\n- Trending Green Open Heart Dial\r\n- IGP Colour coating with no fading & rusting.\r\n- *100% Quality & Reliable Japanese premium automatic machinery",
+        "details": "",
+        "tags": "tommy,formal,men,chain",
+        "price": {
+            "old": "₹3800",
+            "new": "₹2300"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-modern-automatic-generic-a013abece7c4/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-modern-automatic-generic-a013abece7c4/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-modern-automatic-generic-a013abece7c4/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-modern-automatic-generic-a013abece7c4/other2.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tommy%20Hilfiger%20Modern%20Automatic%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dtommy-hilfiger-modern-automatic-generic-a013abece7c4%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tommy%20Hilfiger%20Modern%20Automatic%20Generic%20%0ASKU%3A%20Tom%0APrice%3A%20%E2%82%B92300%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
