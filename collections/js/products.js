bb3b27b22879eeebd3cad34c77ef387555ae16d5
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-07 14:48:04 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4457,5 +4457,28 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Armani%20Exchange%20Hampton%20Automatic%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Darmani-exchange-hampton-automatic-generic-b22d404c7c16%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Armani%20Exchange%20Hampton%20Automatic%20Generic%20%0ASKU%3A%20Armani%20%0APrice%3A%20%E2%82%B92200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Hublot Classic Fusion Automatic Generic ",
+        "slug": "hublot-classic-fusion-automatic-generic-b96e4b83c4f7",
+        "sku": "Hublot ",
+        "brand": "Hublot ",
+        "badge": "Hot",
+        "availability": "In stock ",
+        "description": "Working 12 hour analog\r\n- Finished and Polished stainless steel case\r\n- 6 H-shaped Screw\r\n- Water Resistant\r\n- Blue smart fit adjustable strap\r\n- Stainless Steel Deployant Buckle Clasp\r\n- Silver Stainless Steel Case IGP Colour (No fading or rusting) \r\n- *High End quality Japanese premium self winding automatic machinery",
+        "details": "",
+        "tags": "hublot,formal,men,leather",
+        "price": {
+            "old": "₹4700",
+            "new": "₹3200"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/hublot-classic-fusion-automatic-generic-b96e4b83c4f7/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/hublot-classic-fusion-automatic-generic-b96e4b83c4f7/hover.webp",
+            "others": []
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Hublot%20Classic%20Fusion%20Automatic%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dhublot-classic-fusion-automatic-generic-b96e4b83c4f7%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Hublot%20Classic%20Fusion%20Automatic%20Generic%20%0ASKU%3A%20Hublot%20%0APrice%3A%20%E2%82%B93200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
