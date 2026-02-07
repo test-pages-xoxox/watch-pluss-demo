e4c6013fdfb101aa40530abceafa83e1927d15c1
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-07 14:51:51 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4480,5 +4480,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Hublot%20Classic%20Fusion%20Automatic%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dhublot-classic-fusion-automatic-generic-b96e4b83c4f7%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Hublot%20Classic%20Fusion%20Automatic%20Generic%20%0ASKU%3A%20Hublot%20%0APrice%3A%20%E2%82%B93200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Versace Women's Rose Gold Collection Generic ",
+        "slug": "versace-women-s-rose-gold-collection-generic-6031b6d8b0c6",
+        "sku": "Versace ",
+        "brand": "Versace",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "Working 12 Hour analog\r\n- Stainless steel metal belt\r\n- Elegant Rose Gold Bezel and trending green dial\r\n- Original branding stainless Steel back\r\n- Rose Gold Bracelet Look wrist\r\n- *100% Reliable & High Quality Premium battery operated quartz machinery* ❤️\r\n",
+        "details": "",
+        "tags": "women-chain, women-formal,women",
+        "price": {
+            "old": "₹2300",
+            "new": "₹1700"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/versace-women-s-rose-gold-collection-generic-6031b6d8b0c6/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/versace-women-s-rose-gold-collection-generic-6031b6d8b0c6/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/versace-women-s-rose-gold-collection-generic-6031b6d8b0c6/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Versace%20Women%27s%20Rose%20Gold%20Collection%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dversace-women-s-rose-gold-collection-generic-6031b6d8b0c6%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Versace%20Women%27s%20Rose%20Gold%20Collection%20Generic%20%0ASKU%3A%20Versace%20%0APrice%3A%20%E2%82%B91700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
