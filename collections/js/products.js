33af96cea01ac60d64189770e6096675ffd391d3
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-15 20:35:21 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4771,5 +4771,31 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Guess%20Headline%20Chronograph%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dguess-headline-chronograph-generic-d3ec243def3d%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Guess%20Headline%20Chronograph%20Generic%20%0ASKU%3A%20Gus%0APrice%3A%20%E2%82%B91800%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Tommy Hilfiger Two-Tone Stainless Steel Generic ",
+        "slug": "tommy-hilfiger-two-tone-stainless-steel-generic-7bc2659b2471",
+        "sku": "Tom",
+        "brand": "Tommy Hilfiger",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "Working 3 Hands Analog\r\n- Date Window\r\n- Luminous Hands & Markers\r\n- Rotating Style Bezel\r\n- Two-Tone Stainless Steel Strap (Silver & Gold)\r\n- IGP Colour coating with no fading & rusting.\r\n- *100% Premium, reliable & Trusted quartz movement machinery",
+        "details": "",
+        "tags": "tommy,formal,men,chain",
+        "price": {
+            "old": "₹3800",
+            "new": "₹1800"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-two-tone-stainless-steel-generic-7bc2659b2471/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-two-tone-stainless-steel-generic-7bc2659b2471/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-two-tone-stainless-steel-generic-7bc2659b2471/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/tommy-hilfiger-two-tone-stainless-steel-generic-7bc2659b2471/other2.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tommy%20Hilfiger%20Two-Tone%20Stainless%20Steel%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dtommy-hilfiger-two-tone-stainless-steel-generic-7bc2659b2471%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tommy%20Hilfiger%20Two-Tone%20Stainless%20Steel%20Generic%20%0ASKU%3A%20Tom%0APrice%3A%20%E2%82%B91800%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
