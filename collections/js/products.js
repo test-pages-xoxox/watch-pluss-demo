61fce34d98f11abfab9170329ed3a16a842fa4d8
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-17 11:53:49 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4951,5 +4951,35 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rayban%20%20Polorised%20%20Fiber%20metal%20%20Unisex%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Drayban-polorised-fiber-metal-unisex-generic-5ed327d3b62a%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rayban%20%20Polorised%20%20Fiber%20metal%20%20Unisex%20Generic%20%0ASKU%3A%20Sun%0APrice%3A%20%E2%82%B9950%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Rayban  Pure glass  Metal frame  Unisex  Generic ",
+        "slug": "rayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b",
+        "sku": "Sun",
+        "brand": "Rayban",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "",
+        "details": "",
+        "tags": "sunglasses",
+        "price": {
+            "old": "₹1999",
+            "new": "₹950"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b/other2.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b/other3.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b/other4.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b/other5.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b/other6.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rayban%20%20Pure%20glass%20%20Metal%20frame%20%20Unisex%20%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Drayban-pure-glass-metal-frame-unisex-generic-7ea86b4dc69b%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rayban%20%20Pure%20glass%20%20Metal%20frame%20%20Unisex%20%20Generic%20%0ASKU%3A%20Sun%0APrice%3A%20%E2%82%B9950%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
