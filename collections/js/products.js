5d91a271cb45b7803d5b798ad9c8eee433b65f8a
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-17 11:46:54 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -4924,5 +4924,32 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Hublot%20Gen%C3%A8ve%20Chronograph%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dhublot-gen-ve-chronograph-generic-240cc0fd0559%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Hublot%20Gen%C3%A8ve%20Chronograph%20Generic%20%0ASKU%3A%20Hub%0APrice%3A%20%E2%82%B92100%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Rayban  Pure glass  Unisex Generic ",
+        "slug": "rayban-pure-glass-unisex-generic-aab5057d3748",
+        "sku": "Sun",
+        "brand": "Rayban",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "",
+        "details": "",
+        "tags": "sunglasses",
+        "price": {
+            "old": "₹1999",
+            "new": "₹950"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-unisex-generic-aab5057d3748/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-unisex-generic-aab5057d3748/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-unisex-generic-aab5057d3748/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-unisex-generic-aab5057d3748/other2.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/rayban-pure-glass-unisex-generic-aab5057d3748/other3.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rayban%20%20Pure%20glass%20%20Unisex%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Drayban-pure-glass-unisex-generic-aab5057d3748%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rayban%20%20Pure%20glass%20%20Unisex%20Generic%20%0ASKU%3A%20Sun%0APrice%3A%20%E2%82%B9950%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
