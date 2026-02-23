1278f5591ee578f215967144c6ddc3dedddd0b5b
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-23 23:37:46 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5210,5 +5210,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20New%20balance%209060%20triple%20black%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dnew-balance-9060-triple-black-generic-6110e385344c%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20New%20balance%209060%20triple%20black%20Generic%20%0ASKU%3A%20Shoe%0APrice%3A%20%E2%82%B92700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Nike Air90 solar flare Generic ",
+        "slug": "nike-air90-solar-flare-generic-ebdc2d32f1ea",
+        "sku": "Shoe",
+        "brand": "Nike",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "*Nike Air90 solar flare*\r\n\r\n*Size:41 to 44*\r\n\r\n*Semi UA quality Imported",
+        "details": "",
+        "tags": "shoes",
+        "price": {
+            "old": "₹4700",
+            "new": "₹2500"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/nike-air90-solar-flare-generic-ebdc2d32f1ea/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/nike-air90-solar-flare-generic-ebdc2d32f1ea/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/nike-air90-solar-flare-generic-ebdc2d32f1ea/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Nike%20Air90%20solar%20flare%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dnike-air90-solar-flare-generic-ebdc2d32f1ea%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Nike%20Air90%20solar%20flare%20Generic%20%0ASKU%3A%20Shoe%0APrice%3A%20%E2%82%B92500%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
