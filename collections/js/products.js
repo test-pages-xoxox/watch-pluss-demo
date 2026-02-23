d5ad23f1cf12468717e2038b53e388d935fa2d7f
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-23 17:02:44 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5163,27 +5163,27 @@ const products = [
     },
     {
         "id": "#",
-        "name": "Nike Airforce1 brown Generic ",
-        "slug": "nike-airforce1-brown-generic-d3b7654db93a",
-        "sku": "Sho",
-        "brand": "Nike ",
+        "name": "New balance 9060 natural indigo blue Generic ",
+        "slug": "new-balance-9060-natural-indigo-blue-generic-c805c18a4f97",
+        "sku": "Shoe",
+        "brand": "Balance ",
         "badge": "Trending ",
         "availability": "In stock ",
-        "description": "*Nike Airforce1 brown*\r\n\r\n*Size:41 to 45*\r\n\r\n*Semi UA quality Imported*",
+        "description": "*New balance 9060 natural indigo blue*\r\n\r\n*Size:41 to 45*\r\n\r\n*Semi UA quality Imported*\r\n",
         "details": "",
         "tags": "shoes",
         "price": {
-            "old": "₹4700",
-            "new": "₹2600"
+            "old": "₹5800",
+            "new": "₹2800"
         },
         "images": {
-            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/nike-airforce1-brown-generic-d3b7654db93a/main.webp",
-            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/nike-airforce1-brown-generic-d3b7654db93a/hover.webp",
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/new-balance-9060-natural-indigo-blue-generic-c805c18a4f97/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/new-balance-9060-natural-indigo-blue-generic-c805c18a4f97/hover.webp",
             "others": [
-                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/nike-airforce1-brown-generic-d3b7654db93a/other1.webp"
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/new-balance-9060-natural-indigo-blue-generic-c805c18a4f97/other1.webp"
             ]
         },
-        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Nike%20Airforce1%20brown%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dnike-airforce1-brown-generic-d3b7654db93a%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
-        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Nike%20Airforce1%20brown%20Generic%20%0ASKU%3A%20Sho%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20New%20balance%209060%20natural%20indigo%20blue%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dnew-balance-9060-natural-indigo-blue-generic-c805c18a4f97%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20New%20balance%209060%20natural%20indigo%20blue%20Generic%20%0ASKU%3A%20Shoe%0APrice%3A%20%E2%82%B92800%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
