5103d5d9a7a17e0536c5bbce69f6bb626c29a8b1
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-02-26 19:36:55 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5235,5 +5235,28 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Nike%20Air90%20solar%20flare%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dnike-air90-solar-flare-generic-ebdc2d32f1ea%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Nike%20Air90%20solar%20flare%20Generic%20%0ASKU%3A%20Shoe%0APrice%3A%20%E2%82%B92500%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "  Imported Louis vuittion Size 12-9 approx  Generic ",
+        "slug": "imported-louis-vuittion-size-12-9-approx-generic-be49d03918d8",
+        "sku": "Bag",
+        "brand": "Louis ",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "",
+        "details": "",
+        "tags": "bags",
+        "price": {
+            "old": "₹3800",
+            "new": "₹1999"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/imported-louis-vuittion-size-12-9-approx-generic-be49d03918d8/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/imported-louis-vuittion-size-12-9-approx-generic-be49d03918d8/hover.webp",
+            "others": []
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20%20%20Imported%20Louis%20vuittion%20Size%2012-9%20approx%20%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dimported-louis-vuittion-size-12-9-approx-generic-be49d03918d8%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20%20%20Imported%20Louis%20vuittion%20Size%2012-9%20approx%20%20Generic%20%0ASKU%3A%20Bag%0APrice%3A%20%E2%82%B91999%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
