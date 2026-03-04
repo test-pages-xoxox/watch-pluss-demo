f30b6803ba815e0a01a69eaf010c6ba18668649a
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-03-04 23:12:07 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5308,5 +5308,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Fossil%20Privateer%20Automatic%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dfossil-privateer-automatic-generic-e2e64d78522c%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Fossil%20Privateer%20Automatic%20Generic%20%0ASKU%3A%20Fossil%20%0APrice%3A%20%E2%82%B92200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "G-Shock GA-2100-1A2ER Generic ",
+        "slug": "g-shock-ga-2100-1a2er-generic-a98514316137",
+        "sku": "G-Shock ",
+        "brand": "G-Shock ",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "For Unisex\r\n# 7AA Premium Collection\r\n# Model - GA-2100-1A2ER\r\n# Case size (L× W× H)\r\n48.5 × 45.4 × 11.8 mm \r\n\r\n- Water and impact Resistant \r\n- Countdown timer\r\n- Mineral Glass\r\n- Digital drive System\r\n- Black Neon Blue Face\r\n- Working StopWatch/ Day/Date Digital Display ✅\r\n- Smart fit Fibre strap ✅\r\n- Easy Pin Buckled Lock ✅\r\n- Fibre Body & Bezel",
+        "details": "",
+        "tags": "men,formal,leather",
+        "price": {
+            "old": "₹3200",
+            "new": "₹1300"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/g-shock-ga-2100-1a2er-generic-a98514316137/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/g-shock-ga-2100-1a2er-generic-a98514316137/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/g-shock-ga-2100-1a2er-generic-a98514316137/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20G-Shock%20GA-2100-1A2ER%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dg-shock-ga-2100-1a2er-generic-a98514316137%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20G-Shock%20GA-2100-1A2ER%20Generic%20%0ASKU%3A%20G-Shock%20%0APrice%3A%20%E2%82%B91300%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
