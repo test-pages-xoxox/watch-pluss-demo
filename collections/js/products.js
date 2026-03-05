f319c4a42a40763811eadabf6b9913a3a9801fc8
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-03-05 12:57:47 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5458,5 +5458,31 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tissot%20Supersport%20Chrono%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dtissot-supersport-chrono-generic-9eb58a83e660%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tissot%20Supersport%20Chrono%20Generic%20%0ASKU%3A%20Tissot%20%0APrice%3A%20%E2%82%B92100%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "OMEGA Speedmaster Reduced Generic ",
+        "slug": "omega-speedmaster-reduced-generic-24b226111a2c",
+        "sku": "Omega",
+        "brand": "Omega ",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "Features:-👇\r\n• Quartz movement \r\n• ⁠All chronographs working \r\n• ⁠Tachymeter bezel\r\n• ⁠Luminous hands\r\n• ⁠Premium Build \r\n• ⁠Ss back & Bfly lock\r\n",
+        "details": "",
+        "tags": "omega,formal,chain,men",
+        "price": {
+            "old": "₹4700",
+            "new": "₹1800"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/omega-speedmaster-reduced-generic-24b226111a2c/main.webp",
+            "hover": "",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/omega-speedmaster-reduced-generic-24b226111a2c/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/omega-speedmaster-reduced-generic-24b226111a2c/other2.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20OMEGA%20Speedmaster%20Reduced%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Domega-speedmaster-reduced-generic-24b226111a2c%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20OMEGA%20Speedmaster%20Reduced%20Generic%20%0ASKU%3A%20Omega%0APrice%3A%20%E2%82%B91800%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
