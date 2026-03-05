b4fb0080654e85ce531648bb8368ffc9a1e46d91
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-03-05 23:54:03 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5583,5 +5583,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Guess%20Men%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dguess-men-s-generic-3ffdc3e30744%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Guess%20Men%27s%20Generic%20%0ASKU%3A%20Guess%20%0APrice%3A%20%E2%82%B92200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Gucci Women's Generic ",
+        "slug": "gucci-women-s-generic-0447a8513228",
+        "sku": "Guess ",
+        "brand": "Guess ",
+        "badge": "Hot",
+        "availability": "In stock ",
+        "description": "Brand - Guess \r\n# For - Her \r\n# Model - Fame\r\n# Quality - 7AA Premium Collection \r\n# Model No. - GW0859L1\r\n# Guess Origanal Design & Model\r\n\r\n- Working 3 Hands Analog\r\n- Black dial with logo design\r\n- Black Braslet\r\n- Glass: Mineral Crystal\r\n- Crystal-embellished bezel\r\n- IGP Colour coating with no fading & rusting.\r\n- *100% quality & reliable quartz machinery* 💯",
+        "details": "",
+        "tags": "women-chain, women-formal,women",
+        "price": {
+            "old": "₹2300",
+            "new": "₹1400"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/gucci-women-s-generic-0447a8513228/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/gucci-women-s-generic-0447a8513228/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/gucci-women-s-generic-0447a8513228/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Gucci%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dgucci-women-s-generic-0447a8513228%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Gucci%20Women%27s%20Generic%20%0ASKU%3A%20Guess%20%0APrice%3A%20%E2%82%B91400%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
