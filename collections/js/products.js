b7856cb87c4d65618c49e990d03afb7eb6a70aaf
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-03-07 11:56:36 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5608,5 +5608,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Gucci%20Women%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dgucci-women-s-generic-0447a8513228%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Gucci%20Women%27s%20Generic%20%0ASKU%3A%20Guess%20%0APrice%3A%20%E2%82%B91400%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Jordan retro4 military Generic ",
+        "slug": "jordan-retro4-military-generic-f8696c9ba6e3",
+        "sku": "Shoe",
+        "brand": "Jordan",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "",
+        "details": "*Jordan retro4 military black*\r\n\r\n*Size:41 to 44*\r\n\r\n*Semi UA quality Imported*",
+        "tags": "shoes",
+        "price": {
+            "old": "₹4700",
+            "new": "₹2400"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/jordan-retro4-military-generic-f8696c9ba6e3/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/jordan-retro4-military-generic-f8696c9ba6e3/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/jordan-retro4-military-generic-f8696c9ba6e3/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Jordan%20retro4%20military%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Djordan-retro4-military-generic-f8696c9ba6e3%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Jordan%20retro4%20military%20Generic%20%0ASKU%3A%20Shoe%0APrice%3A%20%E2%82%B92400%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
