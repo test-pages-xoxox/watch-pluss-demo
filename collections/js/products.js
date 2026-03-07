c00fd84c25029b6f51b9baa3dd6874e72d775f47
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-03-07 12:01:18 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5633,5 +5633,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Nike%20airforce1%20Orewood%20moon%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dnike-airforce1-orewood-moon-generic-ce7735e42994%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Nike%20airforce1%20Orewood%20moon%20Generic%20%0ASKU%3A%20Shoe%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Nike airforce1 Paris black Generic ",
+        "slug": "nike-airforce1-paris-black-generic-37fd6ea78e98",
+        "sku": "Shoe",
+        "brand": "Nike",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "*Nike airforce1 Paris black*\r\n\r\n*Size:41 to 45*\r\n\r\n*Semi UA quality Imported*",
+        "details": "",
+        "tags": "shoes",
+        "price": {
+            "old": "₹4700",
+            "new": "₹2600"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/nike-airforce1-paris-black-generic-37fd6ea78e98/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/nike-airforce1-paris-black-generic-37fd6ea78e98/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/nike-airforce1-paris-black-generic-37fd6ea78e98/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Nike%20airforce1%20Paris%20black%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dnike-airforce1-paris-black-generic-37fd6ea78e98%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Nike%20airforce1%20Paris%20black%20Generic%20%0ASKU%3A%20Shoe%0APrice%3A%20%E2%82%B92600%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
