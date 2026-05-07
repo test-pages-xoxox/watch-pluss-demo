71c8e9a6d7b47de3f6e406cfa5b299997e79ac5a
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-05-07 11:44:14 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5811,5 +5811,32 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Rolex%20Rainbow%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Drolex-rainbow-generic-5833d6f23a5d%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Rolex%20Rainbow%20Generic%20%0ASKU%3A%20rolex%20%0APrice%3A%20%E2%82%B92200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "G-Shock Chain strap Generic ",
+        "slug": "g-shock-chain-strap-generic-cbaad12468c6",
+        "sku": "G-Shock ",
+        "brand": "G-Shock ",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "G-Shock Chain strap watch all crono and date working ",
+        "details": "",
+        "tags": "formal,chain,men",
+        "price": {
+            "old": "₹3800",
+            "new": "₹2100"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/g-shock-chain-strap-generic-cbaad12468c6/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/g-shock-chain-strap-generic-cbaad12468c6/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/g-shock-chain-strap-generic-cbaad12468c6/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/g-shock-chain-strap-generic-cbaad12468c6/other2.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/g-shock-chain-strap-generic-cbaad12468c6/other3.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20G-Shock%20Chain%20strap%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dg-shock-chain-strap-generic-cbaad12468c6%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20G-Shock%20Chain%20strap%20Generic%20%0ASKU%3A%20G-Shock%20%0APrice%3A%20%E2%82%B92100%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
