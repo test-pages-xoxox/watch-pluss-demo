f642503128108bc54da763605e0f08f93732eba0
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-06-13 20:22:16 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -5943,5 +5943,31 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Jacob%20%26%20Co%20Rudra%20Edition%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Djacob-co-rudra-edition-generic-67432d5f7839%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Jacob%20%26%20Co%20Rudra%20Edition%20Generic%20%0ASKU%3A%20Jacob%20%0APrice%3A%20%E2%82%B92200%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Ap Swatch Generic ",
+        "slug": "ap-swatch-generic-fb9cad5b7640",
+        "sku": "Audemars",
+        "brand": "Audemars Piguet",
+        "badge": "Trending ",
+        "availability": "In stock ",
+        "description": "Ap Swatch Full Demanding Article Now Available",
+        "details": "",
+        "tags": "audemars,formal,men",
+        "price": {
+            "old": "₹4700",
+            "new": "₹1700"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/ap-swatch-generic-fb9cad5b7640/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/ap-swatch-generic-fb9cad5b7640/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/ap-swatch-generic-fb9cad5b7640/other1.webp",
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/ap-swatch-generic-fb9cad5b7640/other2.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Ap%20Swatch%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dap-swatch-generic-fb9cad5b7640%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Ap%20Swatch%20Generic%20%0ASKU%3A%20Audemars%0APrice%3A%20%E2%82%B91700%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
