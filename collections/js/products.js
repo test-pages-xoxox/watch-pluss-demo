e7a158415887549b62b81c7488d0c734756f5680
test-pages-xoxox <umer.qureshi.work@gmail.com>
2026-06-21 16:28:13 +0530

    Update products.js from Gist

--- collections/js/products.js
+++ collections/js/products.js
@@ -6568,5 +6568,30 @@ const products = [
         },
         "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Tommy%20Hilfiger%20Men%27s%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dtommy-hilfiger-men-s-generic-095b29695234%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
         "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Tommy%20Hilfiger%20Men%27s%20Generic%20%0ASKU%3A%20Tommy%20%0APrice%3A%20%E2%82%B92100%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
+    },
+    {
+        "id": "#",
+        "name": "Guess Top Quality Generic ",
+        "slug": "guess-top-quality-generic-89b5e88ef3fe",
+        "sku": "Men ",
+        "brand": "Guess ",
+        "badge": "Hot ",
+        "availability": "In stock ",
+        "description": "All high \r\nGuess\r\n100% japn mumnt \r\nLadies watch \r\nAll crno warking ",
+        "details": "",
+        "tags": "men,formal,chain",
+        "price": {
+            "old": "₹7800",
+            "new": "₹4500"
+        },
+        "images": {
+            "main": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/guess-top-quality-generic-89b5e88ef3fe/main.webp",
+            "hover": "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/guess-top-quality-generic-89b5e88ef3fe/hover.webp",
+            "others": [
+                "https://raw.githubusercontent.com/test-pages-xoxox/watch-pluss-demo/master/images/products/watch/guess-top-quality-generic-89b5e88ef3fe/other1.webp"
+            ]
+        },
+        "whatsappUrl": "https://wa.me/918097949357?text=Hello%21%20%F0%9F%91%8B%0A%0AI%27m%20interested%20in%20your%20product%3A%20Guess%20Top%20Quality%20Generic%20.%0AProduct%20link%3A%0Ahttps%3A//watchpluss.in/product-detail.html%3Fpid%3Dguess-top-quality-generic-89b5e88ef3fe%0ACould%20you%20please%20provide%20more%20details%20or%20confirm%20availability%3F%0A%0AThank%20you%21%20%F0%9F%99%8F",
+        "whatsappDetailed": "https://wa.me/918097949357?text=Product%3A%20Guess%20Top%20Quality%20Generic%20%0ASKU%3A%20Men%20%0APrice%3A%20%E2%82%B94500%0AAvailability%3A%20In%20stock%20%0A%0APlease%20confirm%20my%20order."
     }
 ];
